80c2a8f3ae423e39b8de5135373f872a73697e81
Toby Caulk <tobycaulk@gmail.com>
2023-11-29 08:29:18 -0500

    Add jest support

---
+++ localmerch-api/babel.config.js
@@ -0,0 +1,6 @@
+export const presets = [
+    ['@babel/preset-env', {
+        targets: { node: 'current' }
+    }],
+    '@babel/preset-typescript'
+];
--- localmerch-api/build/index.js
+++ localmerch-api/build/index.js
@@ -1,2 +1,7 @@
 "use strict";
-console.log('Hello World');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const server_1 = __importDefault(require("./infrastructure/server"));
+server_1.default.start();
